bdb5f7b8d1b8ee64c43ffe14e49eace28c373121
raphadrummond <raphavenanciodrummond@gmail.com>
2021-12-08 21:54:00 -0300

    Trabalhando com RecyclerView e seu Adapter
    --- Voltar a Activity
    
    private fun...
    rvList = findViewById(R.id.rv_list)
    
    --- criar uma classe file kotlin com nome "MenuItemAdapter"
    class MenuItemAdapter : RecyclerView.Adapter<MenuItemAdapter.MenuItemAdapterViewHolder> {
     class MenuItemAdapterViewHolder(val itemView: View) : RecyclerView.ViewHolder
    
    --- Implementar os 3 que são pedidos

---
+++ app/src/main/java/com/dnommurd/projetobancario/MenuItemAdapter.kt
@@ -0,0 +1,24 @@
+package com.dnommurd.projetobancario
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MenuItemAdapter : RecyclerView.Adapter<MenuItemAdapter.MenuItemAdapterViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuItemAdapterViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MenuItemAdapterViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class MenuItemAdapterViewHolder(val itemView: View) : RecyclerView.ViewHolder }
+}
+}
